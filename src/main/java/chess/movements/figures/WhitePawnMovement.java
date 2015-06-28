16d55c0bf4ec48d59976f0bb1a5e03002185d0e5
Tomas Rehak <trehak1@gmail.com>
2015-06-28 20:05:51 +0200

    moves

---
+++ src/main/java/chess/movements/figures/WhitePawnMovement.java
@@ -0,0 +1,95 @@
+package chess.movements.figures;
+
+import chess.board.Board;
+import chess.enums.Col;
+import chess.enums.Coord;
+import chess.enums.Figure;
+import chess.enums.Row;
+import chess.movements.EnPassant;
+import chess.movements.Move;
+import chess.movements.Promotion;
+
+/**
+ * Created by Tom on 28.6.2015.
+ */
+class WhitePawnMovement extends PawnMovement {
+
+    WhitePawnMovement(Col col, Row row, Board board) {
+        super(board, row, col);
+    }
+
+
+    @Override
+    public Move forwardByOne() {
+        if (row == Row._7) {
+            return null;
+        }
+        if (isEmpty(col, row.north())) {
+            return new Move(Coord.get(col, row), Coord.get(col, row.north()));
+        }
+        return null;
+    }
+
+    @Override
+    public Move forwardByTwo() {
+        if (row != Row._2) {
+            return null;
+        }
+        if (isEmpty(col, row.north())) {
+            if (isEmpty(col, row.north().north())) {
+                return new Move(Coord.get(col, row), Coord.get(col, row.north().north()));
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Move captures() {
+        if (col.east().isValid() && row.north().isValid()) {
+            if (isEnemy(col.east(), row.north())) {
+                return new Move(Coord.get(col, row), Coord.get(col.east(), row.north()));
+            }
+        }
+        if (col.west().isValid() && row.north().isValid()) {
+            if (isEnemy(col.west(), row.north())) {
+                return new Move(Coord.get(col, row), Coord.get(col.west(), row.north()));
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public EnPassant enPassants() {
+        if (col.east().isValid()) {
+            if (board.isEnPassantAllowed(col.east(), row)) {
+                return new EnPassant(Coord.get(col, row), Coord.get(col.east(), row));
+            }
+        }
+        if (col.west().isValid()) {
+            if (board.isEnPassantAllowed(col.west(), row)) {
+                return new EnPassant(Coord.get(col, row), Coord.get(col.west(), row));
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Promotion promotion() {
+        if (row == Row._7) {
+            if (isEmpty(col, row.north())) {
+                return new Promotion(Coord.get(col, row), Coord.get(col, row.north()));
+            }
+        }
+        return null;
+    }
+
+
+    private boolean isEmpty(Col col, Row row) {
+        return board.get(col, row) == Figure.NONE;
+    }
+
+    private boolean isEnemy(Col col, Row row) {
+        return board.get(col, row) != Figure.NONE && board.get(col, row).getPlayer() != player;
+    }
+
+}
