a9aad354ef7c7def906549afe608b9b17bf85104
Tomas Rehak <trehak1@gmail.com>
2015-07-01 22:24:58 +0200

    perft start

---
+++ src/test/java/chess/perft/PerftTest.java
@@ -0,0 +1,17 @@
+package chess.perft;
+
+import org.junit.Test;
+
+/**
+ * Created by Tom on 1.7.2015.
+ */
+public class PerftTest {
+
+    private static final int[] perft = new int[]{20, 400};
+
+    @Test
+    public void perftTest() {
+        
+    }
+
+}
