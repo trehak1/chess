36e9fa3b4c16d6c3209852cf0c99d7ffe00d89a4
Matej Ferenc <Matej.Ferenc@4finance.com>
2015-07-02 11:37:30 +0200

    pawn movements tests

--- src/test/java/chess/movements/PawnMovementsTest.java
+++ src/test/java/chess/movements/PawnMovementsTest.java
@@ -12,7 +12,9 @@ import java.util.List;
 /**
  * Created by Tom on 28.6.2015.
  */
-public class PawnMovementsTest {
+public class PawnMovementsTest extends MovementsTest {
+
+    private PawnMovements pawnMovements = new PawnMovements(Player.WHITE);
 
     @Test
     public void testWhites() {
@@ -28,6 +30,20 @@ public class PawnMovementsTest {
         PawnMovements pawnMovements = new PawnMovements(Player.BLACK);
         List<Movement> movements = pawnMovements.getMovements(board);
         Assert.assertEquals(16, movements.size());
+    }
+
+    @Test
+    public void testEnPassant() {
+        testNumbersOfPossibleMovements("pawn/enPassant.txt", 2);
+    }
+
+    @Test
+    public void testBlockedPawn() {
+        testNumbersOfPossibleMovements("pawn/blockedPawn.txt", 0);
+    }
+
+    private void testNumbersOfPossibleMovements(String fileName, int number) {
+        testNumbersOfPossibleMovements(fileName, pawnMovements, number);
     }
 
 }
