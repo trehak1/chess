225c31c76720e2424f1bd3ad370870a763a0210b
Matej Ferenc <Matej.Ferenc@4finance.com>
2015-07-03 11:38:02 +0200

    Merge branch 'master' of https://github.com/trehak1/chess
    Conflicts:
    	src/test/java/chess/perft/Perft.java

--- src/test/java/chess/perft/Perft.java
+++ src/test/java/chess/perft/Perft.java
@@ -15,39 +15,48 @@ import java.util.Set;
 
 public class Perft {
 
-	private static final long[] PERFT = new long[]{20, 400, 8902, 197281, 4865609, 119060324, 3195901860L, 84998978956L, 2439530234167L, 69352859712417L};
-	
-	public long perft(int iterations) throws InterruptedException {
-		Board newGame = new BoardFactory().newGameBoard();
-		Player player = Player.WHITE;
-		final List<Board> boards = Lists.newArrayList(newGame);
-		Date start = new Date();
-		System.out.println("Start: "+start);
-		for (int i = 0; i < iterations; i++) {
-			Set<Movement> allMoves = getAllMoves(boards, player);
-			Assert.assertEquals(PERFT[i], allMoves.size());
-			boards.clear();
-			allMoves.forEach((m) -> boards.add(m.getResultingBoard()));
-			player = player.enemy();
-			System.out.println("iteration: " + (i+1) + ", boards: " + allMoves.size() + ", completed on " + new Date());
-			if (i == iterations - 1) {
-				return allMoves.size();
-			}
-		}
-		return -1;
-	}
+    public static final long[] PERFT = new long[]{20, 400, 8902, 197281, 4865609, 119060324, 3195901860L, 84998978956L, 2439530234167L, 69352859712417L};
+    public static final long[] PERFT_POSITION_2 = new long[]{20, 400, 8902, 197281, 4865609, 119060324, 3195901860L, 84998978956L, 2439530234167L, 69352859712417L};
 
-	private Set<Movement> getAllMoves(List<Board> boards, Player player) throws InterruptedException {
-		Set<Movement> moves = Sets.newConcurrentHashSet();
-		MovementFactory factory = MovementFactory.getFor(player);
-		boards.parallelStream().forEach((b) -> moves.addAll(factory.getMoves(b)));
-		return moves;
-	}
+    private Board board;
+    private Player player;
+    private long[] perfts;
 
-	public static void main(String[] args) throws InterruptedException {
-		Perft perft = new Perft();
-		long res = perft.perft(4);
-		System.out.println(res);
-	}
+    public Perft(Board board, Player player, long[] perfts) {
+        this.board = board;
+        this.player = player;
+        this.perfts = perfts;
+    }
+
+    public long perft(int iterations) throws InterruptedException {
+        final List<Board> boards = Lists.newArrayList(board);
+        Date start = new Date();
+        System.out.println("Start: "+start);
+        for (int i = 0; i < iterations; i++) {
+            Set<Movement> allMoves = getAllMoves(boards, player);
+            Assert.assertEquals(perfts[i], allMoves.size());
+            boards.clear();
+            allMoves.forEach((m) -> boards.add(m.getResultingBoard()));
+            player = player.enemy();
+            System.out.println("iteration: " + (i+1) + ", boards: " + allMoves.size() + ", completed on " + new Date());
+            if (i == iterations - 1) {
+                return allMoves.size();
+            }
+        }
+        return -1;
+    }
+
+    private Set<Movement> getAllMoves(List<Board> boards, Player player) throws InterruptedException {
+        Set<Movement> moves = Sets.newConcurrentHashSet();
+        MovementFactory factory = MovementFactory.getFor(player);
+        boards.parallelStream().forEach((b) -> moves.addAll(factory.getMoves(b)));
+        return moves;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Perft perft = new Perft(new BoardFactory().newGameBoard(), Player.WHITE, PERFT);
+        long res = perft.perft(5);
+        System.out.println(res);
+    }
 
 }
